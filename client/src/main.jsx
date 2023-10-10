dddffa6876c412d9fc9ff79b4732cd6e2dc1aaef
Sathvik Reddy <sathvik5883@gmail.com>
2023-10-11 00:13:43 +0530

    redirectURL changed

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     domain="dev-3i44qg4wbynsgp0r.us.auth0.com"
     clientId="O5ibBTeW3biWWsQSjm1xUgbBqXxG12eE"
     authorizationParams={{
-      redirect_uri:"http://localhost:5173/"
+      redirect_uri:"https://real-estate-website-client-sage.vercel.app"
     }}
     >
     <App />
